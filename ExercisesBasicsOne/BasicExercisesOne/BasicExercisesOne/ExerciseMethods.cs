5e2409271f12509548b17036cbe8a6cf22c7111b
JayMackay <jared.mackay1994@live.com>
2020-02-19 16:31:51 +0000

    SumArray & EvenOdd Methods With Unit Tests

---
+++ ExercisesBasicsOne/BasicExercisesOne/BasicExercisesOne/ExerciseMethods.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace BasicExercisesOne
+{
+    public class ExerciseMethods
+    {
+        // implement this method so it returns the sum of all the entries in the array nums
+        public static int SumArray(int[] nums)
+        {
+            int total = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                total += nums[i];
+            }
+            return total;
+        }
+
+        public static bool EvenOdd(int num)
+        {
+            if (num % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ ExercisesBasicsOne/BasicExercisesOne/MethodUnitTests/SumArrayTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using BasicExercisesOne;
+
+namespace MethodUnitTests
+{
+    public class SumArrayTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void WhenTheArrayIsPopulatedTheSumIsCorrect()
+        {
+            int[] nums = { 3, 7, 1, 2, 4 };
+            var result = ExerciseMethods.SumArray(nums);
+            Assert.AreEqual(17, result);
+        }
+
+        [Test]
+        public void WhenTheArrayIsEmptyTheSumIsZero()
+        {
+            int[] nums = { };
+            var result = ExerciseMethods.SumArray(nums);
+            Assert.AreEqual(0, result);
+        }
+    }
+}
